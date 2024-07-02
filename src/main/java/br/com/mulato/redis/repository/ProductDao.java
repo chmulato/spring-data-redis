0ad852e1aa9ea929c4c85efd67f4f53ad04c6942
Christian Mulato <chrvlamulato@rd.com.br>
2024-07-02 09:07:41 -0300

    Ajuste

--- src/main/java/br/com/mulato/redis/repository/ProductDao.java
+++ src/main/java/br/com/mulato/redis/repository/ProductDao.java
@@ -12,22 +12,27 @@ public class ProductDao {
 
 	public static final String HASH_KEY = "Product";
 
+	@SuppressWarnings("rawtypes")
 	@Autowired
 	private RedisTemplate template;
 
+	@SuppressWarnings("unchecked")
 	public Product save(Product product) {
 		template.opsForHash().put(HASH_KEY, product.getId(), product);
 		return product;
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Product> findAll() {
 		return template.opsForHash().values(HASH_KEY);
 	}
 
+	@SuppressWarnings("unchecked")
 	public Product findProductById(int id) {
 		return (Product) template.opsForHash().get(HASH_KEY, id);
 	}
 
+	@SuppressWarnings("unchecked")
 	public String deleteProduct(int id) {
 		template.opsForHash().delete(HASH_KEY, id);
 		return "product removed !!";
